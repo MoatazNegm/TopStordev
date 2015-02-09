2aff3e2226e166590088ab4fb5ad1ab3e6778002
MezoAmazon <MezoAmazon@gmail.com>
2015-02-09 13:40:06 +0200

    added user Privileges in Priv. will propagate to all add and delete scripts

---
+++ privthis.sh
@@ -0,0 +1,14 @@
+#! /usr/local/bin/zsh
+# needed the operands to be like : one:two:three:four
+userreq=` echo $@ | awk '{print $2}'`;
+modpriv=` echo $@ | awk '{print $1}'`;
+superuser='admin';
+if [[ $superuser == $userreq ]];
+then
+  echo true;
+else
+userpriv=` cat txt/userpriv.txt | grep $userreq`;
+rr=$modpriv\":\";
+priv=`echo ${userpriv##*"$rr"} | awk -F\" '{print $1}'`;
+echo $priv;
+fi
