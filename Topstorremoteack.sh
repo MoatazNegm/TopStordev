a3e2121d0b6a7c49b9f1eb8e8555d3ad332ceecb
MezoAmazon <MezoAmazon@gmail.com>
2015-03-03 12:49:38 +0200

    added Remoterequest.sh

--- Topstorremoteack.sh
+++ Topstorremoteack.sh
@@ -2,6 +2,6 @@
 cd /TopStor
 while true; do
 {
-nc -l 2235 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD ;
+nc -l 2235 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD;
 }
 done;
