8fb5f62df1eb286f599c10234124d91c7b2ffd89
MezoAmazon <MezoAmazon@gmail.com>
2015-03-03 13:22:10 +0200

    fixing replication step 1

--- Remoterequest.sh
+++ Remoterequest.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/zsh
 cd /TopStor
 echo 10.3.2.8 welcome | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  10.3.2.7 2234
-read line < /tmp/msgrack ;
+read -t 20 line < /tmp/msgrack ;
 echo line=$line;
--- Topstor.sh
+++ Topstor.sh
@@ -1,17 +1,19 @@
 #!/usr/local/bin/zsh
 cd /TopStor
 mkfifo -m 660 /tmp/msgfile
-mkfifo -m 660 /tmp/ackmsg;
+mkfifo -m 600 /tmp/msgrack;
 mkfifo -m 600 /tmp/msgremotefile;
 ./Topstorremote.sh &
-chgrp moataz /tmp/msgfile; chgrp moataz /tmp/ackmsg;
-chown www /tmp/msgfile; chown www /tmp/ackmsg
+./Topstorremoteack.sh > /tmp/msgrack &
+chgrp moataz /tmp/msgfile; 
+chown www /tmp/msgfile; 
 rm /TopStor/txt/*
 ClearExit() {
 	echo got a signal > /TopStor/txt/sigstatus.txt
 	rm /tmp/msgfile
-	rm /tmp/ackmsg;
+	rm /tmp/msgrack;
 	rm /tmp/msgremotefile;
+	ps -aux | grep /bin/nc | kill -9
 	exit 0;
 }
 trap ClearExit HUP
