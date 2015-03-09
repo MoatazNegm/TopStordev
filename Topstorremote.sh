a1e63a8896c4edb28cd9e0f94cea9f559375a671
MezoAmazon <MezoAmazon@gmail.com>
2015-03-09 20:44:30 +0200

    trying topstorremote.sh to tmp/msgfile

--- Topstorremote.sh
+++ Topstorremote.sh
@@ -8,26 +8,24 @@ ClearExit() {
 trap ClearExit HUP
 while true; do 
 {
-echo listening
 nc -l 2234 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD > /tmp/msgremotefile & 
 read line < /tmp/msgremotefile;
-echo newdata;
 echo $line > /TopStor/tmplineremote
+stamp=`echo $line | awk '{print $2}'`
 request=`echo $line | awk '{print $1}'`
 searsource=` cat partners.txt | grep "$reqeust"`; 
 ispartner=`echo $searsource | wc -c `
 #echo $line $searsource | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & 
 if [[ $searsource == $request ]]; then
-reqparam=`echo $line | cut -d " " -f2-`;
-echo trusted ;
-echo reqparam:$reqparam 
-echo Authorized:$reqparam $searsource $request | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 
-./pump.sh $reqparam ;
-echo $reqparam > msgfiletmp
+stamp=`echo $line | awk '{print $2}'`;
+reqparam=`echo $line | awk '{$1=$2=""; print substr($0,3) }'`;
+echo $stamp Authorized $reqparam $searsource $request | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 ;
+./dummy.sh $reqparam > /tmp/msgfile; 
+echo $reqparam > msgfiletmp;
 sleep 1;
 #echo done | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & ;
 else
-echo Not Authorized:$line | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & ;
+echo $stamp Not Authorized:$line | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & ;
 fi
 }
 done;
