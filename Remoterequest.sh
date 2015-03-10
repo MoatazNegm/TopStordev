6cff048b03bf2a20fcaa13c23a53684b4ca7c229
MezoAmazon <MezoAmazon@gmail.com>
2015-03-10 22:54:12 +0200

    made the first time replication well but using existing snapshot which is wrong

--- Remoterequest.sh
+++ Remoterequest.sh
@@ -2,14 +2,15 @@
 cd /TopStor
 so=`echo $@ | awk '{print $1}'`;
 dst=`echo $@ | awk '{print $2}'`;
-msg=`echo $@ | awk '{$1=$2=""; print}'`
+reply=`echo $@ | awk '{print $3}'`;
+msg=`echo $@ | awk '{$1=$2=$3=""; print substr($0,4)}'`
 lineflag="failed"
 stamp=`date +%s`
 flagcount=0;
 while [[ $lineflag == "failed" && flagcount -le 5 ]]; do
 {
 
-echo $so $stamp $msg | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $dst  2234
+echo $so $stamp $reply $msg | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $dst  2234
 read -t 3 line < /tmp/msgrack ;
 stampreply=`echo $line | awk '{print $1}'`;
 flagcount=$((flagcount+1));
