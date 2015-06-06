2b52211cc17b0d1170bc17ac86d7e3ccd07bd05b
MezoAmazon <MezoAmazon@gmail.com>
2015-06-06 18:39:00 +0200

    solved stability and connectivity availability

--- ProxyReCloseSVC.sh
+++ ProxyReCloseSVC.sh
@@ -9,21 +9,21 @@ while true;  do
  issocat=`ps -auxw | grep -w socat | grep -w "$ppplus" | wc -c `;
  if [[ $issocat -le 3 ]];
  then
-  /usr/local/bin/socat PIPE:txt/cin$ppplus TCP4:$Proxy:$ppplus &
-  echo /usr/local/bin/socat PIPE:txt/cin$ppplus TCP4:$Proxy:$ppplus >> txt/tmpsocat
+  /usr/local/bin/socat PIPE:txt/cin$ppplus TCP4:$Proxy:$ppplus,forever,reuseaddr &
+  echo /usr/local/bin/socat PIPE:txt/cin$ppplus TCP4:$Proxy:$ppplus,forever,reuseaddr >> txt/tmpsocat
 
  fi
  issocat=`ps -auxw | grep -w socat | grep -w "$pp" | wc -c `;
  if [[ $issocat -le 3 ]];
  then
-  /usr/local/bin/socat TCP4:$Proxy:$pp PIPE:txt/cout$pp &
-  echo /usr/local/bin/socat  TCP4:$Proxy:$pp PIPE:txt/cout$pp >> txt/tmpsocat
+  /usr/local/bin/socat TCP4:$Proxy:$pp,forever,reuseaddr PIPE:txt/cout$pp &
+  echo /usr/local/bin/socat TCP4:$Proxy:$pp,forever,reuseaddr PIPE:txt/cout$pp >> txt/tmpsocat
  fi;
  issocat=`ps -auxw | grep -w socat | grep -w "$ppzfs" | wc -c `;
  if [[ $issocat -le 3 ]];
  then
-  /usr/local/bin/socat  TCP4:$Proxy:$ppzfs PIPE:txt/c$ppzfs &
-  echo /usr/local/bin/socat  TCP4:$Proxy:$ppzfs PIPE:txt/c$ppzfs >> txt/tmpsocat
+  /usr/local/bin/socat TCP4:$Proxy:$ppzfs,forever,reuseaddr PIPE:txt/c$ppzfs &
+  echo /usr/local/bin/socat  TCP4:$Proxy:$ppzfs,forever,reuseaddr PIPE:txt/c$ppzfs >> txt/tmpsocat
  fi;
  sleep 1
 done
