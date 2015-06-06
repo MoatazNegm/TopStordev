938a775c056303731275d9609a3781954b8c0bb2
MezoAmazon <MezoAmazon@gmail.com>
2015-06-06 18:39:41 +0200

    solved stability and connectivity of connection

--- ProxyReplicateSVC.sh
+++ ProxyReplicateSVC.sh
@@ -4,24 +4,24 @@ so=`echo $@ | awk '{print $1}'`;
 dst=`echo $@ | awk '{print $2}'`;
 pp=`echo $@ | awk '{print $5}'`;
 porplus=$((pp+1));
+ppzfs=$((pp+2));
 msg=`echo $@ | awk '{$1=$2=""; print substr($0,3)}'`
 proxyser=`cat proxy.txt | awk '{print $1}'`;
 license=` cat proxy.txt | awk '{print $2}'`;
 lineflag="failed"
-mkfifo txt/cin$pp txt/cout$pp
 while true; do
  issocat=`ps -auxw | grep -w socat | grep -w "$pp" | wc -c `;
- if [[ issocat -le 3 ]]; 
+ if [[ $issocat -le 3 ]]; 
  then
   stamp=`date +%s`
   read -t 2  line</tmp/msgrack ;
   echo initporxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy >> txt/tmpproxyrequest
   echo $so $stamp $dst $license $msg  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N $proxyser 2234
-  echo echo $so $stamp $dst $license $msg  \| openssl enc -a -A -aes-256-cbc -k SuperSecretPWD \| gzip -cf \| nc -N $proxyser 2234 >> txt/tmpproxyrequest
+#  echo echo $so $stamp $dst $license $msg  \| openssl enc -a -A -aes-256-cbc -k SuperSecretPWD \| gzip -cf \| nc -N $proxyser 2234 >> txt/tmpproxyrequest
   sleep 2
-  echo /usr/local/bin/socat TCP4:$proxyser:$pp,reuseaddr GOPEN:txt/cin$pp >>txt/tmpproxyrequest 
-#  echo /usr/local/bin/socat TCP4:$proxyser:$pp,reuseaddr GOPEN:txt/cin$pp 
-  /usr/local/bin/socat TCP4:$proxyser:$pp,reuseaddr GOPEN:txt/cin$pp &
+  echo /usr/local/bin/socat -u TCP4:$proxyser:$pp,forever,reuseaddr PIPE:txt/cin$pp >>txt/tmpproxyrequest 
+#  echo /usr/local/bin/socat -u TCP4:$proxyser:$pp GOPEN:txt/cin$pp 
+  /usr/local/bin/socat   PIPE:txt/cin$pp  TCP4:$proxyser:$pp,forever,reuseaddr &
  fi
  porplusin=`ps -auxw | grep -w socat | grep -w $porplus | wc -c`;
  porplusinn=$((porplusin+0));
@@ -29,8 +29,19 @@ while true; do
  then
   echo $so $stamp $dst $license $msg  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N $proxyser 2234
  sleep 2
-   echo /usr/local/bin/socat -u TCP4:$proxyser:$porplus,reuseaddr GOPEN:txt/cout$pp 2>txt/socatit >> txt/tmpproxyrequest
-# echo   /usr/local/bin/socat -u TCP4:$proxyser:$porplus,reuseaddr GOPEN:txt/cout$pp 
-   /usr/local/bin/socat -u TCP4:$proxyser:$porplus,reuseaddr GOPEN:txt/cout$pp &
+  echo initporxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy >> txt/tmpproxyrequest
+   echo /usr/local/bin/socat  TCP4:$proxyser:$porplus,forever,reuseaddr PIPE:txt/cout$pp >> txt/tmpproxyrequest 
+   /usr/local/bin/socat  TCP4:$proxyser:$porplus,forever,reuseaddr PIPE:txt/cout$pp &
+ fi;
+ ppzfsin=`ps -auxw | grep -w socat | grep -w "$ppzfs" | wc -c`;
+ ppzfsinn=$((ppzfsin+0));
+ if [[ $ppzfsin -le 3 ]]; 
+ then
+  echo $so $stamp $dst $license $msg  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N $proxyser 2234
+ sleep 2
+#   echo /usr/local/bin/socat  TCP4:$proxyser:$porplus,forever,reuseaddr GOPEN:txt/cout$pp 2>txt/socatit >> txt/tmpproxyrequest
+  echo initporxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy >> txt/tmpproxyrequest
+ echo   /usr/local/bin/socat  TCP4:$proxyser:$ppzfs,forever,reuseaddr PIPE:txt/c$ppzfs >> txt/tmpproxyrequest
+   /usr/local/bin/socat  PIPE:txt/c$ppzfs TCP4:$proxyser:$ppzfs,forever,reuseaddr &
  fi;
 done
