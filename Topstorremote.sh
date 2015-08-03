862f7c189789bcebaae20f2aaae5c5a69036b6d4
Charlie Root <root@RepliProxy.quickstor.net>
2015-08-03 22:52:19 +0300

    fixing proxy fix2

--- Topstorremote.sh
+++ Topstorremote.sh
@@ -6,42 +6,15 @@ ClearExit() {
 	exit 0;
 }
 trap ClearExit HUP
-/sbin/sysctl net.inet.tcp.msl=2500 
+/sbin/sysctl net.inet.tcp.msl=2500 >/dev/null
 while true; do
 {
-nc -l 2234 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD > /tmp/msgremotefile & 
+nc -ld 2234 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD > /tmp/msgremotefile & 
 read line < /tmp/msgremotefile;
 echo $line > /TopStor/tmplineremote;
- remoteinit=` echo $line | grep ProxyInit | wc -c `;
+ remoteinit=` echo $line | grep Proxy | wc -c `;
 if [[ $remoteinit -ge 3 ]]; then
  ./remoteReceived;
-else
- stamp=`echo $line | awk '{print $2}'`
- request=`echo $line | awk '{print $1}'`
- searsource=` cat partners.txt | grep "$request" | awk '{print $1}'`; 
- echo $request $searcsource > txt/tmpreq
- ispartner=`echo $searsource | wc -c `
- #echo $line $searsource | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & 
- if [[ $searsource == $request ]]; then
-  stamp=`echo $line | awk '{print $2}'`;
-  reply=`echo $line | awk '{print $3}'`;
-  reqparam=`echo $line | awk '{$1=$2=$3=""; print substr($0,4) }'`;
-  instr=`echo $reqparam | awk '{print $1 }'`;
-  oper=`echo $reqparam | awk '{$1=""; print substr($0,2) }'`;
-  if [[ $reply == "rep" ]];
-  then 
-   res=`./$instr $oper`;
-   echo $stamp Authorized $res | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 ;
-  else
-   ./$instr $oper;
-   echo $stamp Authorized received | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 ;
-  fi
-  echo $reqparam > msgfiletmp;
-  sleep 1;
-  #echo done | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & ;
- else
-  echo $stamp Not Authorized:$line | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -N  $request 2235 & ;
- fi
 fi
 }
 done;
