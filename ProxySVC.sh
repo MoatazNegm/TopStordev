c4f6967c1245d1eb2ba57e5bff18298aa09238dd
Charlie Root <root@Replistor.simplegroup>
2015-09-02 13:58:58 +0300

    fixing addtime and ProxySVC

--- ProxySVC.sh
+++ ProxySVC.sh
@@ -1,11 +1,11 @@
 #!/usr/local/bin/zsh
 cd /TopStor
-openvpnflag=0
+openvpnflag=0;
 stamp=`date +%s`;
 proxycurrent=`cat proxy.txt | awk '{print $1}'`;
 proxyname=`cat proxy.txt | awk '{print $3}'`;
 isproxy="";
-pingrouter=0
+pingrouter=`cat txt/pingrouter 2>/dev/null`;
 while true;
 do
  proxyser=`cat proxy.txt | awk '{print $1}'`;
@@ -25,92 +25,106 @@ do
   proxyname=$newproxy;
   ./pump.sh clearSVC all new;
  fi
- while read line;
- do
-  isproxy=`echo $line | awk '{print $3}'`;
-  if [[ $isproxy != "true" ]]; then; localrep="local"; else localrep="proxy";fi
-  dst=`echo $line | awk '{print $1}'`;
-  if [[ $localrep == "proxy" ]];
-  then
-   passphrase=`echo $line | awk '{print $4}'`;
-   pp=`echo $line | awk '{print $5}'`;
-   so=` cat proxy.txt | awk '{print $3}'`;
-   tun=`(ifconfig tun0 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
-  else
-   passphrase="pass";
-   pp=`cat workingpplocal | awk '{print $1}'`;
-   tun=`(ifconfig em1 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
-   so=$tun
-  fi 
-  if [[ -n $tun ]];
-  then
-   if [[ $localrep == "proxy" ]];
+ isrepli=`cat partners.txt`
+ if [[ -z $isrepli ]]; then sleep 10; else
+  while read line;
+  do
+   isproxy=`echo $line | awk '{print $3}'`;
+   if [[ $isproxy != "true" ]]; then; localrep="local"; 
+   else 
+    localrep="proxy";
+     /sbin/ping -c 10 $proxyser >/dev/null 2>&1
+     if [[ $? -ne 0 ]];
+     then
+      proxyrun=0; sleep 5;
+     else
+      proxyrun=1; 
+     fi
+   fi
+   dst=`echo $line | awk '{print $1}'`;
+   if [[ $localrep == "proxy" && $proxyrun -ne 1 ]];
    then
-    ps -auxw | grep ProxyncSVC > /dev/null 2>&1;
-    if [[ $? -ne 0 ]];
-    then
-     ./ProxyncSVC $pp $tun peer &;
-    fi
-   fi
-   otherask=`ps -awx | grep Askrcv | grep "$tun" | grep "$pp" | grep -w "$stamp"`
-   if [[ $? -ne 0 ]]; then
-    ./Askrcv $pp $tun $so $stamp $localrep &;
-   fi
-  fi
-  istun=`echo $tun | awk -F. '{print $1}'`;
-  istunn=$((istun+1))
-  if [[ $istunn -ge 5 ]];
-  then
-   if [[ $localrep == "proxy" ]];
-   then
-    ps -auxw | grep ProxyncSVC > /dev/null 2>&1;
-    if [[ $? -ne 0 ]];
-    then
-     ./ProxyncSVC $pp $tun peer &;
-    fi
-    router=`echo $tun | awk -F. '{print $1"."$2"."$3".1"}'`
-    /sbin/ping -c 10 $router >/dev/null 2>&1
-    if [[ $? -ne 0 ]];
-    then
-     if [[ pingrouter -eq 1 ]]; then echo routerproblem >tmpProxySVC; ./pump.sh clearSVC all no ping router; fi
-    else
-     pingrouter=1;
-    fi
-   fi
-  fi
-  if [[ $localrep == "proxy" ]];
-  then
-   if [[ $openvpnflag -eq 0 ]];
-   then 
-    echo $so $stamp $dst $license ProxyInit $so $pp $passphrase $dst  \| openssl enc -a -A -aes-256-cbc -k SuperSecretPWD \| gzip -cf \| nc -w 4 -N $proxyser 2234 > tmpproxyinit
-    echo $so $stamp $dst $license ProxyInit $so $pp $passphrase $dst  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -w 4 -N $proxyser 2234 2>/dev/null
+    passphrase=`echo $line | awk '{print $4}'`;
+    pp=`echo $line | awk '{print $5}'`;
+    so=` cat proxy.txt | awk '{print $3}'`;
+    tun=`(ifconfig tun0 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
+   else
+    passphrase="pass";
+    pp=`cat workingpplocal | awk '{print $1}'`;
+    tun=`(ifconfig em1 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
+    so=$tun
    fi 
-   ispid=`ps -axw | grep openvpn | grep "$dst"`
-   ispidn=`echo $ispid | wc -c `
-#   if [[ -a txt/$pp && $ispidn -ge 5 ]]; then kill -KILL `echo $ispid | awk '{ print $1}'`; fi
-   if [[ -a txt/$pp || $ispidn -le 3 ]];
+   if [[ -n $tun ]];
    then
-    rm txt/$pp 2>/dev/null;  
-    proxy=$proxyser
-    port=$pp;
-    sed -e "s/PROXY/$proxy/g" -e "s/PORT/$port/g" openvpn.conf > txt/${so}_${dst}_openvpn.conf;
-    /usr/local/sbin/openvpn --daemon $dst --cd /TopStor/txt --config ${so}_${dst}_openvpn.conf; 
-    sleep 10;
+    if [[ $localrep == "proxy" && $proxyrun -ne 1 ]];
+    then
+     ps -auxw | grep ProxyncSVC > /dev/null 2>&1;
+     if [[ $? -ne 0 ]];
+     then
+      ./ProxyncSVC $pp $tun peer &;
+     fi
+    fi
+    otherask=`ps -awx | grep Askrcv | grep "$tun" | grep "$pp" | grep -w "$stamp"`
+    if [[ $? -ne 0 ]]; then
+     ./Askrcv $pp $tun $so $stamp $localrep &;
+    fi
    fi
-  fi
-  if [[ $istunn -ge 5 ]];
-  then
-   otherask=`ps -awx | grep Askrcv | grep "$tun" | grep -vw "$pp" | grep -vw "$stamp"`
-   if [[ $? -eq 0 ]]; then 
-    kill -KILL ` echo $otherask | awk '{print $1}'` >/dev/null 2>&1 ;
+   istun=`echo $tun | awk -F. '{print $1}'`;
+   istunn=$((istun+1))
+   if [[ $istunn -ge 5 ]];
+   then
+    if [[ $localrep == "proxy" && $proxyrun -ne 1 ]];
+    then
+     ps -auxw | grep ProxyncSVC > /dev/null 2>&1;
+     if [[ $? -ne 0 ]];
+     then
+      ./ProxyncSVC $pp $tun peer &;
+     fi
+     router=`echo $tun | awk -F. '{print $1"."$2"."$3".1"}'`
+     /sbin/ping -c 10 $router >/dev/null 2>&1
+     if [[ $? -ne 0 ]];
+     then
+      pingrouter=`cat txt/pingrouter`;
+      if [[ pingrouter -eq 1 ]]; then echo routerproblem >tmpProxySVC; echo 0 > txt/pingrouter; ./pump.sh clearSVC all no ping router;  fi
+     else
+      pingrouter=1;
+     fi
+    fi
    fi
-  fi
-  sleep 5;
-  tun=`(ifconfig tun0 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
-  if [[ $localrep == "proxy" && -n $tun ]];
-  then
-   echo $so $stamp $dst $license ProxyUpdate $so $pp $passphrase $dst  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -w 4 -N $proxyser 2234 2>/dev/null
-  ;
-  fi
- done < partners.txt;
+   if [[ $localrep == "proxy" && $proxyrun -ne 1 ]];
+   then
+    if [[ $openvpnflag -eq 0 ]];
+    then 
+     echo $so $stamp $dst $license ProxyInit $so $pp $passphrase $dst  \| openssl enc -a -A -aes-256-cbc -k SuperSecretPWD \| gzip -cf \| nc -w 4 -N $proxyser 2234 > tmpproxyinit
+     echo $so $stamp $dst $license ProxyInit $so $pp $passphrase $dst  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -w 4 -N $proxyser 2234 2>/dev/null
+    fi 
+    ispid=`ps -axw | grep openvpn | grep "$dst"`
+    ispidn=`echo $ispid | wc -c `
+#    if [[ -a txt/$pp && $ispidn -ge 5 ]]; then kill -KILL `echo $ispid | awk '{ print $1}'`; fi
+    if [[ -a txt/$pp || $ispidn -le 3 ]];
+    then
+     rm txt/$pp 2>/dev/null;  
+     proxy=$proxyser
+     port=$pp;
+     sed -e "s/PROXY/$proxy/g" -e "s/PORT/$port/g" openvpn.conf > txt/${so}_${dst}_openvpn.conf;
+     /usr/local/sbin/openvpn --daemon $dst --cd /TopStor/txt --config ${so}_${dst}_openvpn.conf; 
+     sleep 10;
+    fi
+   fi
+   if [[ $istunn -ge 5 ]];
+   then
+    otherask=`ps -awx | grep Askrcv | grep "$tun" | grep -vw "$pp" | grep -vw "$stamp"`
+    if [[ $? -eq 0 ]]; then 
+     kill -KILL ` echo $otherask | awk '{print $1}'` >/dev/null 2>&1 ;
+    fi
+   fi
+   sleep 5;
+   tun=`(ifconfig tun0 | grep -w 'inet' | awk '{print $2}') 2>/dev/null`
+   if [[ $localrep == "proxy" && -n $tun && $proxyrun -ne 1 ]];
+   then
+    echo $so $stamp $dst $license ProxyUpdate $so $pp $passphrase $dst  | openssl enc -a -A -aes-256-cbc -k SuperSecretPWD | gzip -cf | nc -w 4 -N $proxyser 2234 2>/dev/null
+   ;
+   fi
+  done < partners.txt;
+ fi
 done;
--- addtime.sh
+++ addtime.sh
@@ -4,79 +4,74 @@ export PRE; export POST; export SEARCHY;
 function searchdevice() {
  device=$1
  logs=$2
- devices=`cat $logs | /usr/local/bin/jq '.[]|.[] | .name'| sed 's:"::' | sed 's:"::'`
- json=`cat $logs | /usr/local/bin/jq  -c '.[]'`
- json1=`echo $json | /usr/local/bin/jq -c '.[]'`
- json2=`echo "{\"device\":[\n"$json1"\n]}"`
- echo $devices | grep -w $device  > /dev/null 2>&1
- if [ $? -ne 1 ]
+ logcont=`cat $logs`;
+ echo $logcont | grep -w $device  > /dev/null 2>&1
+ if [[ $? -ne 1 ]];
  then
-  search=`echo $json2 | grep -w $device`
-  pre=`echo $json2 | grep -w -B99999999 $device| grep -v -w $device | tr "\n" "," | sed 's:\[,:\[:'`
-  post=`echo $json2 | grep -w -A99999999 $device| grep -v -w $device| tr "\n" ","|sed 's:^:,:'|sed 's:,]},$:]}:'`
+  separat='{"name":"'${device};
+  pre=${logcont%${separat}*};
+  cpre=${#pre};
+  csep=${#separat}; 
+  searchpost=${separat}${logcont:$((csep+cpre))}
+  separat2='}]}]}]}'
+  search=${searchpost%%${separat2}*}${separat2};
+  post=${searchpost:${#search}}
+  if [[ -z $post ]]; then post=']}'; fi
  else
-  json2=`echo "{\"device\":[\n"$json1"\n"`
-  pre=`echo $json2 |tr "\n" "," | sed 's:\[,:\[:'`
   search=""
   post=`echo "]}"`
+  if [[ -z $logcont ]]; then pre='{"device":[';
+  else pre=${logcont:0:-2}',';
+  fi
  fi
- chksearch=` echo $search | wc -c `
- if [[ $chksearch -le 40 ]]; then search=""; fi
  PRE=$pre; POST=$post; SEARCHY=$search
 }
 
 function searchdate() {
  date=$1
  traf=$2
- search1=` echo $@ | awk '{$1=$2=""; print substr($0,3)}'`; 
- dates=`echo $search1 | /usr/local/bin/jq -c '.[]'| awk 'NR==2{printf"%s",$1}'| /usr/local/bin/jq -c '.[]|.[]|.[]|.Date'| sed 's:"::' | sed 's:"::'`
- json=`echo $search1 | /usr/local/bin/jq -c '.[]'| awk 'NR==2{printf"%s",$1}'| /usr/local/bin/jq -c '.[]|.[]|.[]'`
- echo $dates | grep -w $date > /dev/null 2>&1
- if [ $? -ne 1 ]
+ search1=$SEARCHY; 
+ echo $search1 | grep -w $date > /dev/null 2>&1
+ if [[ $? -ne 1 ]];
  then
-  json1=`echo "{\"name\":\"$device\",\"stats\":[{\"Dates\":[\n"$json"\n]}]}"`
-  echo $json1 |grep -w $date | grep -w "times" > /dev/null 2>&1
-  if [ $? -ne 1 ]
-  then
-   search=`echo $json1 | grep -w $date`
-  else
-   search=""
-  fi
-  pre=`echo $json1 | grep -w -B99999999 $date| grep -v -w $date | tr "\n" "," | sed 's:\[,:\[:'`
-  post=`echo $json1 | grep -w -A99999999 $date| grep -v -w $date| tr "\n" ","|sed 's:^:,:'|sed 's:,]}]},$:]}]}:'`
+  search=${search1:0:-4};
+  pre=""
+  post="]}]}"
  else
-  json=`echo $json`
-  pre=`echo "{\"name\":\"$device\",\"stats\":[{\"Dates\":[\n"$json"\n"|tr "\n" ","| sed 's:\[,:\[:' | sed 's:\]},,:]},:'| sed 's:\[,,:[:' `
-  search=`echo "{\"Date\":\"$date\",\"times\":[]}"`
-  post=`echo "]}]}"`
+  pre=${search1:0:-4};
+  search=""
+  post="]}]}"
  fi
- chksearch=` echo $search | wc -c `
- if [[ $chksearch -le 40 ]]; then search=""; fi
  PRE=$pre; POST=$post; SEARCHY=$search
 }
 
 device=` echo $@ | awk '{print $1}'`;
-date=` echo $@ | awk '{print $2}'`;
-traf=` echo $@ | awk '{print $3}'`;
-oper=` echo $@ | awk '{$1=$2=$3="";print substr($0,4)}'`;
+traf=` echo $@ | awk '{print $2}'`;
+stat=`./iostat.sh $device`
+statdate=` echo $stat | awk -F\| '{ print $2 }' | awk '{print $1}'`;
+date=`echo $statdate | awk -F\/ '{print $2"/"$1"/"$3}'`;
+i=`echo $stat | awk -F\| '{ print "time "$3" bw "$4" rs "$5" ws "$6" svct "$7" qlen "$8}' | awk '{$1=$1}{ print }'`;
+oper=` ./jsonthis3.sh $i`;
+
 searchdevice $device $traf;
-search=$SEARCHY; pre=$PRE; post=$POST
+search=$SEARCHY; devpre=$PRE; devpost=$POST
 # first time for this device 
 if [[ -z $search ]];
 then 
  search='{"name":"'$device'","stats":[{"Dates":[{"Date":"'$date'","times":['$oper']}]}]}';
- devpre=$pre; devpost=$post; devsearch=$search
+ devsearch=$search
 else
- devpre=$pre; devpost=$post; devsearch=$search
- echo $pre | grep -w device > /dev/null 2>&1 
- if [[ $? -ne 0 ]]; then devpre='{"device":['$pre; else devpre=$pre; fi;
- searchdate $date $traf $search;
+ devsearch=$search
+ echo $devpre | grep -w device > /dev/null 2>&1 
+ if [[ $? -ne 0 ]]; then devpre='{"device":['$devpre; fi;
+ SEARCHY=$search
+ searchdate $date $traf ;
  devpre=${devpre}$PRE; devpost=${POST}${devpost}
  search=$SEARCHY; 
 # first time for this date in this device
  if [[ -z $search ]];
  then
-  search='{"Date":"'$date'","times":['$oper']}';
+  search=',{"Date":"'$date'","times":['$oper']}';
  else 
   search=${search:0:-2}','$oper']}';
  fi
