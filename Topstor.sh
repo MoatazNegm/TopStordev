3617c6d72d5000d8135502b64ca5b6fd34ae7e0e
Mezo <sales@quickstor.net>
2016-06-25 01:28:33 +0200

    fixing

--- Topstor.sh
+++ Topstor.sh
@@ -7,26 +7,29 @@ mkfifo -m 660 /tmp/msgfile 2>/dev/null
 mkfifo -m 600 /tmp/msgrack 2>/dev/null
 mkfifo -m 600 /tmp/msgremotefile 2>/dev/null
 export REMOTE=Topstor
-./Topstorremote.sh &
-./Topstorremoteack.sh &
+#./Topstorremote.sh &
+#./Topstorremoteack.sh &
 chgrp moataz /tmp/msgfile; 
-chown www /tmp/msgfile; 
+chown apache /tmp/msgfile; 
+echo $$ > /var/run/topstor.pid
 #rm /TopStor/txt/*
 ClearExit() {
 	echo got a signal > /TopStor/txt/sigstatus.txt
 	rm /tmp/msgfile
 	rm /tmp/msgrack;
 	rm /tmp/msgremotefile;
-	ps -aux | grep /bin/nc | kill -9
+	rm /var/run/topstor.pid 
+#	ps -aux | grep /bin/nc | kill -9
 	exit 0;
 }
 trap ClearExit HUP
 while true; do 
 {
 read line < /tmp/msgfile
-echo $line > /TopStor/tmpline
+echo $line > /TopStordata/tmpline
 request=`echo $line | awk '{print $1}'`
-reqparam=`echo $line | cut -d " " -f2-`
+#reqparam=`echo $line | cut -d " " -f2-`
+reqparam=`echo $line | awk '{$1="";print}'`
 
 ./$request $reqparam >/dev/null 2>&1  & 
 }
