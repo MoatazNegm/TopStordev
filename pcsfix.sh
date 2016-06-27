e68ae0405c414c3fef7a31ee82c8fe31599c19fe
Mezo <sales@quickstor.net>
2016-06-27 19:58:47 +0200

    adding watchdog pcsfix.sh

---
+++ pcsfix.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/zsh
+cd /TopStor
+echo $$ > /var/run/pcsfix.pid
+#rm /TopStor/txt/*
+ClearExit() {
+	exit 0;
+}
+trap ClearExit HUP
+while true; do 
+{
+sleep 5
+pcsitems=`pcs resource`;
+echo $pcsitems | grep ocf | grep -v Started  &>/dev/null
+if [ $? -eq 0 ]; then
+ fsvcs=(`echo $pcsitems | grep ocf | grep -v Started | awk '{print $1}'`)
+ for svc in "${fsvcs[@]}"; do
+  pcs resource debug-start $svc
+  pcs resource cleanup $svc
+ done
+ ps -ef | grep Topstor | grep zsh &>/dev/null
+ if [ $? -ne 0 ]; then
+  systemctl stop topstor 
+  systemctl start topstor 
+ fi
+ 
+fi  
+}
+done;
