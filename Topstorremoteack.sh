b2a48d5aa6d04603902e1c70f5e922145d72bca7
Mezo <sales@quickstor.net>
2016-07-03 00:05:11 +0200

    fixing

--- Topstorremoteack.sh
+++ Topstorremoteack.sh
@@ -1,7 +1,18 @@
 #!/usr/local/bin/zsh
 cd /TopStor
+rm /tmp/msgrack 2>/dev/null
+mkfifo -m 600 /tmp/msgrack 2>/dev/null
+echo $$ > /var/run/topstorremoteack.pid
+ClearExit() {
+        echo got a signal > /TopStor/txt/sigstatusremoteack.txt
+        rm /tmp/msgrack;
+        rm /var/run/topstorremoteack.pid
+         exit 0;
+}
+trap ClearExit HUP
+#./ProxySVC.sh &
 while true; do
 {
-nc -ld 2235 | gunzip | openssl enc -d -aes-256-cbc -a -A -k SuperSecretPWD > /tmp/msgrack
+nc -l 2235 --ssl-cert /TopStor/key/TopStor.crt --ssl-key /TopStor/key/TopStor.key  > /tmp/msgrack
 }
 done;
