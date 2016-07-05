d732f91e1c86558b3636470d3f98f95d65774b6d
Mezo <sales@quickstor.net>
2016-07-05 19:28:15 +0200

    fixing

--- Topstor.sh
+++ Topstor.sh
@@ -1,34 +1,25 @@
 #!/usr/local/bin/zsh
 cd /TopStor
-rm /tmp/msgfile 2>/dev/null
-#rm /tmp/msgrack 2>/dev/null
-#rm /tmp/msgremotefile 2>/dev/null
-mkfifo -m 660 /tmp/msgfile 2>/dev/null
-#mkfifo -m 600 /tmp/msgrack 2>/dev/null
-#mkfifo -m 600 /tmp/msgremotefile 2>/dev/null
+rm /tmp2/msgfile 2>/dev/null
+mkdir /tmp2 &>/dev/null
+chown apache /tmp2 &>/dev/null
+mkfifo -m 660 /tmp2/msgfile 2>/dev/null
 export REMOTE=Topstor
-#./Topstorremote.sh &
-#./Topstorremoteack.sh &
-chgrp moataz /tmp/msgfile; 
-chown apache /tmp/msgfile; 
+#chgrp root /tmp/msgfile; 
+chown apache /tmp2/msgfile; 
 echo $$ > /var/run/topstor.pid
-#rm /TopStor/txt/*
 ClearExit() {
 	echo got a signal > /TopStor/txt/sigstatus.txt
-	rm /tmp/msgfile
-#	rm /tmp/msgrack;
-#	rm /tmp/msgremotefile;
+	rm /tmp2/msgfile
 	rm /var/run/topstor.pid 
-#	ps -aux | grep /bin/nc | kill -9
 	exit 0;
 }
 trap ClearExit HUP
 while true; do 
 {
-read line < /tmp/msgfile
+read line < /tmp2/msgfile
 echo $line > /TopStordata/tmpline
 request=`echo $line | awk '{print $1}'`
-#reqparam=`echo $line | cut -d " " -f2-`
 reqparam=`echo $line | awk '{$1="";print}'`
 
 ./$request $reqparam >/dev/null 2>&1  & 
