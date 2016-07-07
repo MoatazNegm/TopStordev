656716f8bdc4d1a35e178bb1b56d27f8bd1b2c4d
Mezo <sales@quickstor.net>
2016-07-07 07:19:40 +0200

    fixing

--- privthis.sh
+++ privthis.sh
@@ -1,5 +1,6 @@
 #!/usr/local/bin/zsh
 # needed the operands to be like : one:two:three:four
+userpriv='/var/www/html/des20/Data/userpriv.txt';
 userreq=` echo $@ | awk '{print $2}'`;
 modpriv=` echo $@ | awk '{print $1}'`;
 superuser='admin';
@@ -7,7 +8,7 @@ if [[ $superuser == $userreq ]];
 then
   echo true;
 else
-userpriv=` cat userpriv.txt | grep "$userreq"`;
+userpriv=` cat $userpriv | grep "$userreq"`;
 rr=$modpriv\":\";
 priv=`echo ${userpriv##*"$rr"} | awk -F\" '{print $1}'`;
 echo $priv;
